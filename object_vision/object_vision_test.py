0eeb9268ef70c99ac945fe74d68b5706a25cbcc9
U-CSCI19571\robots <CSCI19571+robots@CSCI19571.cs.PLU.EDU>
2022-03-25 11:32:43 -0700

    updated object vision to work simultaneously

--- object_vision/object_vision_test.py
+++ object_vision/object_vision_test.py
@@ -10,17 +10,16 @@ thres = 0.45  # Threshold to detect object
 
 kinect = PyKinectRuntime.PyKinectRuntime(PyKinectV2.FrameSourceTypes_Color)
 
-# while (True):
-#     colorFrame = kinect.get_last_color_frame()       # PyKinect2 returns a color frame in a linear array of size (8294400,)
-#     colorFrame = colorFrame.reshape((1080, 1920 ,4)) # pictures are 1920 columns x 1080 rows with 4 bytes (BGRA) per pixel
-#     now = datetime.datetime.now()                    # You know it
-#     cv2.imwrite(now.strftime('temp') + '.jpg',colorFrame) # saves the pictures
-#     cv2.waitKey(0)
-#
-# sleep(15)
 
-img = cv2.imread('temp.jpg')
-
+sleep(2)
+colorFrame = kinect.get_last_color_frame()       # PyKinect2 returns a color frame in a linear array of size (8294400,)
+colorFrame = colorFrame.reshape((1080, 1920 ,4))#.astype(np.uint8) # pictures are 1920 columns x 1080 rows with 4 bytes (BGRA) per pixel
+now = datetime.datetime.now()                    # You know it
+cv2.imwrite(now.strftime("%Y-%m-%d-%H-%M") + '.jpg',colorFrame) # saves the pictures
+img = colorFrame[:, :, :3]
+cv2.imshow("temp",img)
+cv2.waitKey(1)
+sleep(2)
 
 classNames = []
 classFile = "coco.names"
@@ -38,17 +37,20 @@ with open(classFile,"rt") as f:
 
 
 
-    while True:
-        classIds, confs, bbox = net.detect(img, confThreshold=thres) # problem is here
-        print(classIds, bbox)
 
-        if len(classIds) != 0:
-            for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
-                cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-                cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-                cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
-                            cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+    classIds, confs, bbox = net.detect(img, confThreshold=thres) # problem is here
+    print(classIds, bbox)
 
-        cv2.imshow("Output", img)
-        cv2.waitKey(1)
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            img = np.array(img)
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("Output", img)
+    cv2.waitKey(1)
+
+sleep(20)
