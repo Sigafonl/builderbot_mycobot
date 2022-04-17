f1ae777ed1325b7b324979ac068b37016d62be7a
Sigafonl <fsigafoos@gmail.com>
2022-04-16 18:57:53 -0700

    Added config files and nessary scripts for unity simulation

---
+++ myCobot_pi/mycobot_trajectory_subscriber.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+    Subscribes to SourceDestination topic.
+    Uses MoveIt to compute a trajectory from the target to the destination.
+    Trajectory is then published to PickAndPlaceTrajectory topic.
+"""
+import rospy
+
+from builderbot_mycobot.msg import MyCobotMoveitJoints
+from moveit_msgs.msg import RobotTrajectory
+
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard:\n%s", data)
+
+
+def listener():
+    rospy.init_node('Trajectory', anonymous=True)
+    rospy.Subscriber("/mycobot_joints", MyCobotMoveitJoints, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    listener()
